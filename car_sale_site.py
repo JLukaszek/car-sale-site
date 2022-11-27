24af085385762ef22bdc669a08a881959f9f4e75
JL63 <jarek8331@gmail.com>
2022-11-27 22:35:38 +0100

    Added 3 basic templates. Added dropdown feature for car names.

--- car_sale_site.py
+++ car_sale_site.py
@@ -1,9 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route("/")
-@app.route("/home")
+@app.route("/", methods=['GET'])
+@app.route("/home", methods=['GET'])
 def home():
-    return "<h1>Main site</h1>"
+    car_types = ['SUVs', 'Trucks', 'Sedans', 'Crossovers', 'Coupes', 'Convertibles', 'Sports Cars', 'Luxury', 'Vans']
+    return render_template('home.html', car_types=car_types)
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
