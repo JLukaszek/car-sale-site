3e540bf75e43b561049a632e8f9f5ccbc360d198
JL63 <jarek8331@gmail.com>
2022-11-28 21:58:11 +0100

    Added fake data to look like real auctions.

--- car_sale_site.py
+++ car_sale_site.py
@@ -1,18 +1,40 @@
-from flask import Flask, render_template, request
-
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
+cars = [
+    {
+        "car_body": "SUVs",
+        "car_brand": "BMW",
+        "car_model": "X5",
+        "mileage": 96700,
+        "price": "$14000",
+    },
+    {
+        "car_body": "Coupe",
+        "car_brand": "Audi",
+        "car_model": "A4",
+        "mileage": 198000,
+        "price": "$10000",
+    },
+    {
+        "car_body": "Estate",
+        "car_brand": "BMW",
+        "car_model": "Series 3",
+        "mileage": 68600,
+        "price": "$11000",
+    }
+]
 
-@app.route("/", methods=['GET'])
-@app.route("/home", methods=['GET'])
+
+@app.route("/")
+@app.route("/home")
 def home():
-    car_types = ['SUVs', 'Trucks', 'Sedans', 'Crossovers', 'Coupes', 'Convertibles', 'Sports Cars', 'Luxury', 'Vans']
-    return render_template('home.html', car_types=car_types)
+    return render_template('home.html', cars=cars)
 
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title='About Page')
 
 
 if __name__ == "__main__":
